f12ca09958f4b7c3f2a5cda578eb1110104a4266
donjoe <giumontanaro@gmail.com>
2021-03-26 11:13:29 +0100

    Hide mongodb URI in env variable

--- dbconnection.js
+++ dbconnection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connectToDB = async () => {
 
     try {
-        await mongoose.connect('mongodb+srv://montyDev_:p8zYXmvnKRmHxiV@cluster0.gjhnc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',  { useNewUrlParser: true })
+        await mongoose.connect(process.env.MONGODB_URI,  { useNewUrlParser: true })
 
         console.log('Connection to mongodb created!')
 
